984fa36b34c76c04714b530976a0e823c278c775
Jonathan Monroy <jmonroybat@gmail.com>
2022-03-30 16:49:46 -0600

    Primer commit

---
+++ src/components/Boton.js
@@ -0,0 +1,20 @@
+import "../styles/Boton.css";
+
+function Boton(props) {
+  const esOperador = (valor) => {
+    return isNaN(valor) && valor !== "." && valor !== "=";
+  };
+
+  return (
+    <button
+      className={`boton-contenedor ${
+        esOperador(props.children) ? "operador" : null
+      }`.trim()}
+      onClick={() => props.manejarClick(props.children)}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+export default Boton;
---
+++ src/components/Pantalla.js
@@ -0,0 +1,7 @@
+import "../styles/Pantalla.css";
+
+const Pantalla = ({ input }) => {
+  return <div className="input">{input}</div>;
+};
+
+export default Pantalla;
